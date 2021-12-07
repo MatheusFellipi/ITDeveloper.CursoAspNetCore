df5f757f1f1add7e5c0908234fad20cc7b1735f6
Mathues Fellipi <matheus.fellipi@outlook.com>
2021-12-07 16:13:05 -0300

    Criado a entitade base

---
+++ src/ITDeveloper.CursoAspNetCore.Domain/Entities/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITDeveloper.CursoAspNetCore.Domain.Entities
+{
+	public class EntityBase
+	{
+		public EntityBase()
+		{
+			this.Id = Guid.NewGuid();
+		}
+		public Guid Id { get; set; }
+	}
+}

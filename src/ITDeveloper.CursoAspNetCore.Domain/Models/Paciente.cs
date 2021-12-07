22379306b2a9f41ea7751614d576c1eb99fe66ae
Mathues Fellipi <matheus.fellipi@outlook.com>
2021-12-07 16:13:45 -0300

    Criando a modal de paciente

---
+++ src/ITDeveloper.CursoAspNetCore.Domain/Models/Paciente.cs
@@ -0,0 +1,36 @@
+﻿using ITDeveloper.CursoAspNetCore.Domain.Entities;
+using ITDeveloper.CursoAspNetCore.Domain.Enums;
+using System;
+
+namespace ITDeveloper.CursoAspNetCore.Domain.Models
+{
+	public class  Paciente : EntityBase
+
+	{
+		public Paciente()
+		{
+			this.DataInternacao = DateTime.Now;
+			this.Ativo = true;
+		}
+		public string Nome { get; set; }
+
+		public DateTime DataInternacao { get; set; }
+
+		public DateTime DataNascimento { get; set; }
+
+		public string Email { get; set; }
+
+		public bool Ativo { get; set; }
+
+		public string Cpf { get; set; }
+
+		public TipoPaciente TipoPaciente { get; set; }
+		public Sexo Sexo { get; set; }
+
+		public string Rg { get; set; }
+
+		public string RgOrgao { get; set; } 
+
+		public DateTime RgDataEmisao { get; set; }
+	}
+}

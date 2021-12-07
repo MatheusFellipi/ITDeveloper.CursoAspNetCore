bc58637af5cda139b10c374a40461f9416e39785
Mathues Fellipi <matheus.fellipi@outlook.com>
2021-12-07 16:14:06 -0300

    Adicionaod a modal de paciente

--- ITDeveloper.CursoAspNetCore.Infra.Data/ORM/ITDeveloperDBContext.cs
+++ ITDeveloper.CursoAspNetCore.Infra.Data/ORM/ITDeveloperDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using ITDeveloper.CursoAspNetCore.Domain.Entities;
+using ITDeveloper.CursoAspNetCore.Domain.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace ITDeveloper.CursoAspNetCore.Infra.Data.ORM
@@ -8,9 +9,10 @@ namespace ITDeveloper.CursoAspNetCore.Infra.Data.ORM
 		public ITDeveloperDBContext(DbContextOptions<ITDeveloperDBContext> options)
 		:base(options)
 		{
-
 		}
 
 		public DbSet<Mural> Mural { get; set; }
-	}
+
+		public DbSet<Paciente> Pacientes { get; set; }
+	}  
 }
